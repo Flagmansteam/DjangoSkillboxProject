9b045b62c89413793b2fc81c30cd8d474fbb7879
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2023-05-12 06:01:03 +0300

    Create shopapp

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_django_project.shopapp.apps.ShopappConfig',
 ]
 
 MIDDLEWARE = [
---
+++ first_django_project/shopapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import shop_index
+
+app_name = "shopapp"
+urlpatterns = [
+    path("",shop_index, name="index")
+]
