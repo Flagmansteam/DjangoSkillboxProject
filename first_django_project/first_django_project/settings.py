e25517c806128e0c29c94da8f5e96746e8bb6278
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2023-05-15 23:59:20 +0300

    Create shopapp 15/05/23

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'first_django_project.shopapp.apps.ShopappConfig',
+    'shopapp.apps.ShopappConfig',
 ]
 
 MIDDLEWARE = [
--- first_django_project/shopapp/views.py
+++ first_django_project/shopapp/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from timeit import default_timer
 
 def shop_index(request:HttpRequest):
-    return render(request, 'shopapp/shop-index.html')
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
 
 # Create your views here.
