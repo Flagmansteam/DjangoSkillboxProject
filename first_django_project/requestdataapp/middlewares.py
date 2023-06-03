1422839fc4c37fd73a36ecb013a2cf8d137add90
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2023-06-04 01:22:02 +0300

    Practic finish #5 03.06.23

--- first_django_project/requestdataapp/middlewares.py
+++ first_django_project/requestdataapp/middlewares.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def set_useragent_on_request_middleware(get_response):
     print("initial call")
+
     def middleware(request: HttpRequest):
         print("before get response")
         request.user_agent = request.META["HTTP_USER_AGENT"]
@@ -17,25 +18,21 @@ def set_useragent_on_request_middleware(get_response):
 
 
 class CountRequestsMiddleware:
-    def __init__(self,get_response):
-         self.get_response = get_response
-         self.request_count = 0
-         self.request_time = {}
-         self.responses_count = 0
-         self.exceptions_count = 0
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.request_count = 0
+        self.responses_count = 0
+        self.exceptions_count = 0
+        self.requests = {}
 
-
-    def __call__(self,request: HttpRequest):
-        time_delay = 10 # min delta between requests
-        if not self.request_time:
-            print('This is the first request of this user after server was restarted')
-        else:
-            if (round(time.time()) * 1) - self.request_time['time'] < time_delay \
-                    and self.request_time['ip_address'] == request.META.get('REMOTE_ADDR'):
+    def __call__(self, request: HttpRequest):
+        ip = request.META.get('REMOTE_ADDR')
+        if ip in self.requests:
+            delta = time.time() - self.requests[ip]
+            if delta < 10:
                 print('It has been less than 10 seconds since the last request from your ip address')
                 return render(request, 'requestdataapp/error-request.html')
-        self.request_time = {'time': round(time.time()) * 1, 'ip-address': request.META.get('REMOTE_ADDR')}
-
+        self.requests[ip] = time.time()
         self.request_count += 1
         print("request count", self.request_count)
         response = self.get_response(request)
@@ -46,6 +43,3 @@ class CountRequestsMiddleware:
     def process_exception(self, request: HttpRequest, exception: Exception):
         self.exceptions_count += 1
         print("got", self.exceptions_count, "exceptions so far")
-
-
-
---
+++ first_django_project/requestdataapp/views.py
@@ -0,0 +1,36 @@
+from django.core.files.storage import FileSystemStorage
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+def process_get_view(request:HttpRequest)-> HttpResponse:
+    a = request.GET.get("a","")
+    b = request.GET.get("b", "")
+    result = a + b
+
+    context = {
+        "a": a,
+        "b": b,
+        "result": result,
+    }
+    return render(request, 'requestdataapp/request-query-params.html', context=context)
+
+def user_form(request:HttpRequest) -> HttpResponse:
+    return render(request, "requestdataapp/user-bio-form.html")
+
+def handle_file_upload(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST" and request.FILES.get("myfile"):
+        myfile = request.FILES["myfile"]
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        #practic work part
+        file_size = fs.size(filename)
+        if file_size > 1048576:  # 1mb
+            fs.delete(filename)
+            print('file is deleted:', filename)
+            return render(request, 'requestdataapp/error-message.html')
+        else:
+            print("save file", filename)
+
+    return render(request,"requestdataapp/file-upload.html")
+
