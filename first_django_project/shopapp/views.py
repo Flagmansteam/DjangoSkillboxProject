3c1939cfe16091fe5c31ee08b0087b24f58d068d
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2023-06-09 15:06:56 +0300

    Practic finish #6 09.06.23 update

--- first_django_project/shopapp/views.py
+++ first_django_project/shopapp/views.py
@@ -36,7 +36,6 @@ def products_list(request: HttpRequest):
     return render(request, 'shopapp/products-list.html', context=context)
 
 def create_product(request:HttpRequest)->HttpResponse:
-    form = ProductForm(request.POST)
     if request.method =="POST":
         form = ProductForm(request.POST)
         if form.is_valid():
@@ -45,23 +44,22 @@ def create_product(request:HttpRequest)->HttpResponse:
             form.save()
             url = reverse("shopapp:products_list")
             return redirect(url)
-        else:
-            form = ProductForm()
+    else:
+        form = ProductForm()
     context = {
         "form": form,
     }
     return render(request, "shopapp/create-product.html", context=context)
 
 def create_order(request:HttpRequest)->HttpResponse:
-    form = OrderForm(request.POST)
     if request.method =="POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             form.save()
             url = reverse("shopapp:orders-list")
             return redirect(url)
-        else:
-            form = OrderForm()
+    else:
+        form = OrderForm()
     context = {
         "form": form,
     }
