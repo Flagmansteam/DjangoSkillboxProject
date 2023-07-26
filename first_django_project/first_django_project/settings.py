33f423980ddae50e333b74049d61d22fee0f9e9a
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2023-07-26 15:37:34 +0300

    Practic 11 26.07.2023

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -124,6 +124,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
+
+LOCALE_PATHS =[
+    BASE_DIR/ 'locale/'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- first_django_project/myauth/views.py
+++ first_django_project/myauth/views.py
@@ -1,19 +1,24 @@
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LogoutView
 from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView, CreateView, ListView
-from .models import Profile  # чтобы у новых пользователей появляля профиль
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView # класс UpdateView даёт функционал для обновления модели
+from .models import Profile
 from .forms import *
 
 
-class AboutMeView(TemplateView):
-    template_name ="myauth/about-me.html"
+class HelloView(View):
+    def get(self,request:HttpRequest)-> HttpResponse:
+        return HttpResponse("<h1>Hello world</h1>")
+
+
+# class AboutMeView(TemplateView):
+#     template_name ="myauth/about-me.html"
 
 class RegisterView(PermissionRequiredMixin, CreateView):
     form_class = UserCreationForm
@@ -86,52 +91,114 @@ def get_session_view(request:HttpRequest)->HttpResponse:
 class FooBarView(View):
     def get(self,request:HttpRequest)->JsonResponse:
         return JsonResponse({"foo":"bar", "spam":"eggs"})
+#==========================================================================
+#==========================================================================
+#==========================================================================
+
+class AboutMeView(UserPassesTestMixin, UpdateView): # Представление для обновления профиля пользователя
+    model = Profile
+    template_name = 'myauth/about-me.html'
+    form_class = UserProfileForm
+    success_url = '/about-me/'
+
+    def get_object(self,queryset=None):
+        return self.request.user.profile
+
+    def test_func(self):
+        return self.request.user.is_authenticated
 
 
-def about_me(request: HttpRequest):
-    user_profile = request.user.userprofile
+class UserListView(ListView):
+    model = Profile
+    template_name = 'myauth/users_list.html'
+    context_object_name = 'users' # Имя переменной, которая будет содержать список пользователей в контексте шаблона
+    queryset = Profile.objects.select_related('user').all() # Запрос для получения списка профилей
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            return redirect('about-me')
-    else:
-        form = UserProfileForm(instance=user_profile)
-        return render(request, 'about-me.html',{'form':form})
+class UserDetailView(UpdateView):# Представление для просмотра и обновления данных пользователя
+    model = Profile
+    template_name = 'myauth/about-me.html'
+    fields = []
+    context_object_name = 'user'
 
-def user_list(request): # получаем все профили и передаём в шаблон
-    users = Profile.objects.select_related('user').all()
-    return render(request, 'myauth/users_list.html', {'users':users})
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user__username=self.kwargs['username'])
 
-# class UserListView(ListView):
-#     model = Profile
-#     template_name = 'users_list.html'
-#     context_object_name = 'users'
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = self.object
+        return context
 
-def user_detail(request,username): # Получаем пользоватеоя и его профиль с помощью имени и передаем в шаблон
-    user = get_object_or_404(User, username=username)
-    profile = get_object_or_404(Profile,user=user)
-    return render(request, 'myauth/about-me.html', {'user':user, 'profile':profile})
+class AvatarUpdateView(UpdateView):
+    model = User
+    template_name = 'myauth/update_avatar.html'
+    fields = []
+    context_object_name = 'user'
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user ==self.get_object()
+
+    def post(self, request,*args,**kwargs):
+        if 'avatar' in request.FILES:
+            avatar = request.FILES['avatar']
+            if avatar.size>2*1024*1024:
+                return HttpResponseBadRequest("Avatar size is too large")
+            if not avatar.content_type.startswith("image/"):
+                return HttpResponseBadRequest("Invalid file type. Only images are allowed")
+
+            user = self.get_object()
+            user.avatar = avatar
+            user.save()
+
+            return HttpResponse("Avatar updated successfully")
+        else:
+            return HttpResponseBadRequest("No avatar file was provided")
 
 
-def update_avatar(request,pk):
-    user = get_object_or_404(User, pk = pk)
-    if not request.user.is_staff and request.user != user:
-        return HttpResponse("You do not have permission to perform this action.")
 
-    if 'avatar' in request.FILES:
-        avatar = request.FILES['avatar']
-        if avatar.size>2*1024*1024:
-            return HttpResponseBadRequest("Avatar size is too large")
-
-        if not avatar.content_type.startswith("image/"):
-            return HttpResponseBadRequest("Invalid file type. Only images are allowed")
-
-        user.avatar = avatar
-        return HttpResponse("Avatar updated successfully")
-
-    else:
-        return HttpResponseBadRequest("No avatar file was provided")
-
-    return render(request, 'myauth/update_avatar.html',{'form':form, 'user':user})
+# def about_me(request: HttpRequest):
+#     user_profile = request.user.userprofile
+#
+#     if request.method == 'POST':
+#         form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('about-me')
+#     else:
+#         form = UserProfileForm(instance=user_profile)
+#         return render(request, 'about-me.html',{'form':form})
+#
+# def user_list(request): # получаем все профили и передаём в шаблон
+#     users = Profile.objects.select_related('user').all()
+#     return render(request, 'myauth/users_list.html', {'users':users})
+#
+# # class UserListView(ListView):
+# #     model = Profile
+# #     template_name = 'users_list.html'
+# #     context_object_name = 'users'
+#
+# def user_detail(request,username): # Получаем пользоватеоя и его профиль с помощью имени и передаем в шаблон
+#     user = get_object_or_404(User, username=username)
+#     profile = get_object_or_404(Profile,user=user)
+#     return render(request, 'myauth/about-me.html', {'user':user, 'profile':profile})
+#
+#
+# def update_avatar(request,pk):
+#     user = get_object_or_404(User, pk = pk)
+#     if not request.user.is_staff and request.user != user:
+#         return HttpResponse("You do not have permission to perform this action.")
+#
+#     if 'avatar' in request.FILES:
+#         avatar = request.FILES['avatar']
+#         if avatar.size>2*1024*1024:
+#             return HttpResponseBadRequest("Avatar size is too large")
+#
+#         if not avatar.content_type.startswith("image/"):
+#             return HttpResponseBadRequest("Invalid file type. Only images are allowed")
+#
+#         user.avatar = avatar
+#         return HttpResponse("Avatar updated successfully")
+#
+#     else:
+#         return HttpResponseBadRequest("No avatar file was provided")
+#
+#     return render(request, 'myauth/update_avatar.html',{'form':form, 'user':user})
